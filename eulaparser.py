9cb101ad7d51037674a373d1285e944e2f11b848
dnfrese <dnfrese@iastate.edu>
2015-09-19 04:43:32 -0500

    Added paraProcessor

--- eulaparser.py
+++ eulaparser.py
@@ -9,6 +9,62 @@ class eulaParagraph:
     header = ''
     paragraph = ''
 
+class paraProcesser(object):
+    def __init__(self, para):
+        self.paragraph = para
+
+    def pprocess(para):
+        fin2 = open('worthless.txt', 'r')
+        lines = para
+        passage = ''
+        for x in lines:
+    	       passage = passage + x
+        passage = passage.replace('\n', '')
+        origSentences = passage.split('.')
+        for x in origSentences:
+            x = x + '.'
+        passage = passage.replace(',', '')
+        sentences = passage.split('.')
+        for x in sentences:
+            x = x.replace('.','')
+        passage = passage.replace('.', '')
+        passage = passage.lower()
+        words = passage.split()
+        wordcount = {}
+        for x in words:
+            if x in wordcount:
+                wordcount[x] += 1
+            else:
+                wordcount[x] = 1
+
+        lines2 = fin2.readlines()
+        for x in range(len(lines2)):
+    	       lines2[x] = lines2[x].replace('\n','')
+        for x in lines2:
+    	       if x in wordcount:
+    	              wordcount[x] = 0
+        sentenceparts = []
+        for x in sentences:
+            sentenceparts.append((x.lower()).split())
+        sentencescore = []
+        for x in sentenceparts:
+            total = 0
+            for y in x:
+                if y in wordcount:
+                    total += wordcount[y]
+            sentencescore.append(total)
+
+        highscore  = 0
+        for x in sentencescore:
+            if x > highscore:
+                highscore = x
+        answer = ''
+        for x in range(len(sentences)):
+            if sentencescore[x] > highscore/2:
+                answer = answer + origSentences[x]
+        fin2.close()
+        return answer
+
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         print "Please enter an EULA file."
@@ -44,7 +100,13 @@ if __name__ == '__main__':
         sections.append(eulaParagraph(heading, para))
         index += 1
 
+    paragrapher = paraProcesser()
+    for p in sections:
+        print "-"*25 + p.header + "-"*25
+        print pararapher.pprocess(p.paragraph)
 
+"""
 for p in sections:
     print "-"*25 + p.header + "-"*25
     print p.paragraph
+"""
