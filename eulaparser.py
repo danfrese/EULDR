b4cd684ffe73b8e0379900cfddb8e7bddf1663d3
dnfrese <dnfrese@iastate.edu>
2015-09-19 18:18:08 -0500

    Added comments to paragraph summarizer

--- eulaparser.py
+++ eulaparser.py
@@ -12,38 +12,45 @@ class eulaParagraph(object):
 
     # breaks down the given paragraph and displays the general idea of it
     def pprocess(self, para):
-        # Opens the worthless dictionary to find
-        fin2 = open('worthless.txt', 'r')
+        fin2 = open('worthless.txt', 'r')   # Opens the worthless dictionary to find useless wording
         lines = para                        # Set lines equal to the current paragraph being analyzed
-        passage = ''                        # Initialize passage to hold the lines to be altered
-        for x in lines:                     # Add paragraph lines to the current passage string
-    	       passage = passage + x
-        passage = passage.replace('\n', '')
-        origSentences = passage.split('.')
-        for x in origSentences:
-            x = x + '.'
-        passage = passage.replace(',', '')
-        sentences = passage.split('.')
-        for x in sentences:
-            x = x.replace('.','')
-        passage = passage.replace('.', '')
-        passage = passage.lower()
-        words = passage.split()
-        wordcount = {}
-        for x in words:
-            if x in wordcount:
-                wordcount[x] += 1
-            else:
-                wordcount[x] = 1
+        passage = ''                        # Initialize passage to hold the lines to be altered for the dictionary
 
-        lines2 = fin2.readlines()
-        for x in range(len(lines2)):
+        # Remove puncutation from the given paragraph
+        for x in lines:                     # Add paragraph lines to the current passage string, useful with multiple paragraphs
+    	       passage += x
+        passage = passage.replace('\n', '') # Remove all newline chars
+        origSentences = passage.split('.')  # Split and remove periods assigning values into an array
+        for x in origSentences:             # Add the removed periods to all indexes
+            x = x + '.'
+        passage = passage.replace(',', '')  # Remove commas from passage
+        sentences = passage.split('.')      # Split and remove periods assigning values into an array
+        for x in sentences:                 # Remove all periods from sentences
+            x = x.replace('.','')
+        passage = passage.replace('.', '')  # Remove periods
+        passage = passage.lower()           # Set all values to lowercase for easy parsing
+
+        # Dictionary counter
+        words = passage.split()             # Split the passage into a giant list of words
+        wordcount = {}                      # Initializes a dictionary literal to hold word counts
+        for x in words:                     # Populates the dictionary literal
+            if x in wordcount:
+                wordcount[x] += 1           # Add count to word
+            else:
+                wordcount[x] = 1            # Sets word count to 1 if not found in dictionary
+
+        # Read in all lines and remove all newline characters
+        lines2 = fin2.readlines()           # Assign all lines of a file to array lines2
+        for x in range(len(lines2)):        # Generate a list of numbers to iterate over
     	       lines2[x] = lines2[x].replace('\n','')
+        # Set all word counts back to 0 to prepare for the next reading
         for x in lines2:
     	       if x in wordcount:
     	              wordcount[x] = 0
+
+        # Split into sentence parts to find the score of a sentence
         sentenceparts = []
-        for x in sentences:
+        for x in sentences:                 # Append the sentence after lowercasing and splitting by spaces
             sentenceparts.append((x.lower()).split())
         sentencescore = []
         for x in sentenceparts:
@@ -53,11 +60,14 @@ class eulaParagraph(object):
                     total += wordcount[y]
             sentencescore.append(total)
 
-        highscore  = 0
+        # Look for the highest score from sentence's score to determine the most high value sentences
+        # A sentence is more valuable if it uses words that appear more frequently in the paragraph
+        highscore = 0
         for x in sentencescore:
             if x > highscore:
                 highscore = x
         answer = ''
+        # Iterate through sentences list to see if the score is higher than the high score#
         for x in range(len(sentences)):
             if sentencescore[x] > highscore/2:
                 answer = answer + origSentences[x]
@@ -109,8 +119,8 @@ if __name__ == '__main__':
         index += 1
         new_heading = heading
 
-#paragrapher = eulaParagraph(new_heading, sections)
+
 paragrapher = eulaParagraph(new_heading, para)
 for p in sections:
     print "-"*25 + p.header + "-"*25            # Prints the current header value
-    print paragrapher.pprocess(p.paragraph)     # This prints the paragraphs inside each heading
+    print paragrapher.pprocess(p.paragraph)     # Prints the simplified paragraph under each heading
